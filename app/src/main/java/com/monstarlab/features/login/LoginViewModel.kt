c8d3e6acb7b34c66b999cbda1e24ea448690d3e2
Lucas Sales <saless182@gmail.com>
2021-02-19 14:28:26 +0100

    Update LoginViewModel.kt

--- app/src/main/java/com/monstarlab/features/login/LoginViewModel.kt
+++ app/src/main/java/com/monstarlab/features/login/LoginViewModel.kt
@@ -4,17 +4,13 @@ import androidx.lifecycle.*
 import com.monstarlab.arch.extensions.LoadingAware
 import com.monstarlab.arch.extensions.ViewErrorAware
 import com.monstarlab.arch.extensions.onSuccess
-import com.monstarlab.core.sharedui.errorhandling.ViewError
-import com.monstarlab.core.sharedui.errorhandling.mapToViewError
-import com.monstarlab.core.usecases.resources.GetResourcesUseCase
 import com.monstarlab.core.usecases.user.LoginUseCase
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.launchIn
 import javax.inject.Inject
 
 class LoginViewModel @Inject constructor(
-        private val loginUseCase: LoginUseCase,
-        private val getResourcesUseCase: GetResourcesUseCase
+    private val loginUseCase: LoginUseCase
 ) : ViewModel(), ViewErrorAware, LoadingAware {
 
     val loginResultFlow: MutableSharedFlow<Boolean> = MutableSharedFlow()
