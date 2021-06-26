48a53ff4a8fdbd66fbc714c17438800a76279eef
Roman Levinzon <levinzon.roman@gmail.com>
2021-06-26 13:46:47 +0200

    feat: add compose theme files

---
+++ app/src/main/java/com/monstarlab/theme/Font.kt
@@ -0,0 +1,15 @@
+package com.monstarlab.theme
+
+import androidx.compose.ui.text.font.FontFamily
+
+val appFontFamily = FontFamily.SansSerif
+
+/*
+val customFontFamily = FontFamily(
+    listOf(
+        Font(R.font.font_res, FontWeight.Normal, FontStyle.Normal),
+        Font(R.font.font_res_light, FontWeight.Light, FontStyle.Normal),
+        Font(R.font.font_res_bold, FontWeight.Bold, FontStyle.Normal),
+    )
+)
+*/
---
+++ app/src/main/java/com/monstarlab/theme/Type.kt
@@ -0,0 +1,67 @@
+package com.monstarlab.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    defaultFontFamily = appFontFamily,
+
+    h1 = TextStyle(
+        fontFamily = appFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp
+    ),
+
+    h2 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp
+    ),
+
+    h3 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp
+    ),
+
+    h5 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+
+    h6 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+
+    subtitle1 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp
+    ),
+
+    subtitle2 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+
+    body1 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+
+    button = TextStyle(
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+)
