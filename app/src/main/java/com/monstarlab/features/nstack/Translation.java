cee7bca3d04ac0d03207dcc59d6bff6ace0281ac
Roman Levinzon <levinzon.roman@gmail.com>
2022-03-30 14:59:17 +0200

    m# Please enter the commit message for your changes. Lines starting

--- app/src/main/java/com/monstarlab/features/nstack/Translation.java
+++ app/src/main/java/com/monstarlab/features/nstack/Translation.java
@@ -22,10 +22,26 @@ public class Translation {
 		public static String skip = "Skip";
 		public static String testtest = "__testtest";
 	}
+	public final static class terms {
+		public static String termsNotFound = "Terms not found.";
+		public static String acceptLabel = "Accept";
+		public static String acceptedLabel = "Accepted";
+	}
+	public final static class more {
+		public static String title = "more";
+	}
+	public final static class rateReminder {
+		public static String rateReminderTitle = "Rate this app";
+		public static String rateReminderMessage = "It seems like you like this app, do you want to give it a rating?";
+		public static String title = "Rate Reminder";
+		public static String firstAction = "First Action";
+		public static String secondAction = "Second Action";
+	}
 	public final static class donate {
 		public static String donateButton = "Donate!";
 		public static String donateAgainButton = "Donate again!";
 		public static String title = "Help the victims now!";
+		public static String acceptLabel = "dwadaw";
 	}
 	public final static class error {
 		public static String authenticationError = "Login expired, please login again.";
@@ -33,5 +49,14 @@ public class Translation {
 		public static String errorTitle = "Error";
 		public static String unknownError = "Unknown error, please try again.";
 		public static String errorRandom = "Random";
+	}
+	public final static class home {
+		public static String title = "NStack Demo";
+		public static String subTitle = "Production";
+		public static String message = "Bacon ipsum dolor amet magna meatball jerky in, shank sunt do burgdoggen spare ribs. Lorem boudin eiusmod short ribs pastrami. Sausage bresaola do turkey, dolor qui tail ground round culpa boudin nulla minim sunt beef ribs ham. Cillum in pastrami adipisicing swine lorem, velit sunt meatloaf bresaola short loin fugiat tri-tip boudin.";
+		public static String toggleOff = "off";
+		public static String sendFeedbackButton = "Send feedback";
+		public static String hint = "Name";
+		public static String toggleOn = "On";
 	}
 }
