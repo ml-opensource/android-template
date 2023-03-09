c3b913af1051e4e0318bafe8963bfe2148cde8e7
Tiago Araujo <taraujodesouza@gmail.com>
2023-03-09 17:05:31 +0100

    feat: added splash api

--- app/src/main/java/com/monstarlab/features/main/MainActivity.kt
+++ app/src/main/java/com/monstarlab/features/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.chuckerteam.chucker.api.Chucker
 import com.monstarlab.R
@@ -12,14 +13,21 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+    private var showSplash = true
     private val notificationRequest =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition { showSplash }
+        init()
+    }
+
+    private fun init() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        setupNStack()
         checkNotificationPermission()
+        setupNStack().invokeOnCompletion { showSplash = false }
     }
 
     private fun checkNotificationPermission() {
