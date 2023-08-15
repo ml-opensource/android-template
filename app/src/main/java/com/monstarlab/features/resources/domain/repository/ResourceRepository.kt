06930043be5fb2d20810b1ce1dbae573fc2d4c88
Roman Levinzon <levinzon.roman@gmail.com>
2023-08-15 14:31:54 +0200

    chore: remove unused code

--- app/src/main/java/com/monstarlab/features/resources/domain/repository/ResourceRepository.kt
+++ app/src/main/java/com/monstarlab/features/resources/domain/repository/ResourceRepository.kt
@@ -1,25 +1,9 @@
 package com.monstarlab.features.resources.domain.repository
 
-import com.monstarlab.features.resources.data.api.ResourcesApi
-import com.monstarlab.features.resources.data.api.dtos.toResource
-import com.monstarlab.features.resources.data.persistence.ResourcePreferenceStore
 import com.monstarlab.features.resources.domain.model.Resource
-import javax.inject.Inject
-
-class ResourceRepositoryImpl @Inject constructor(
-    private val api: ResourcesApi,
-    private val resourcePreferenceStore: ResourcePreferenceStore,
-) :  ResourceRepository {
-
-    override suspend fun get(): List<Resource> {
-        return api.getResources().data.map { it.toResource() }.also {
-            resourcePreferenceStore.addAll(it)
-        }
-    }
-}
 
 interface ResourceRepository {
-    suspend fun get() : List<Resource>
+    suspend fun get(): List<Resource>
 }
 
 
