e3bc4d8223eb14a98cee42aae2ee7303a00aee0b
khairullo <halulkin@gmail.com>
2023-09-12 09:02:03 +0200

    feat: implemented AppModule di class to provide baseUrl from the app module to the core module

---
+++ app/src/main/java/com/monstarlab/AppModule.kt
@@ -0,0 +1,19 @@
+package com.monstarlab
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideBaseUrl(): String {
+        return BuildConfig.API_URL
+    }
+
+}
