21173aadbd4834649bad7f2e7825abb9d9997465
khairullo <halulkin@gmail.com>
2023-09-12 13:40:44 +0200

    feat: updated designsystem module with convention android library plugin

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -2,25 +2,11 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.convention.android.library)
 }
 
 android {
-    compileSdk = 34
     namespace = "com.monstarlab.designsystem"
-    flavorDimensions += "default"
-    defaultConfig {
-        minSdk = 23
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
 }
 
 kotlin {
@@ -30,7 +16,6 @@ kotlin {
 }
 
 dependencies {
-
     // Compose
     implementation(platform(libs.android.compose.bom))
     implementation(libs.bundles.android.compose.core)
