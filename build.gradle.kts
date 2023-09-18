2fe8e86315425bbbde4150071a210b91363943cf
khairullo <halulkin@gmail.com>
2023-09-18 11:10:17 +0200

    build: added android library plugin to the project gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ buildscript {
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.convention.android.library) apply false
     alias(libs.plugins.detekt) apply false
